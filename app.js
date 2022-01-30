fd57a321877c2381470580ebdf7c05900da94d39
whosshazo <ryandeshazo1@gmail.com>
2022-01-30 12:07:11 -0500

    HTML generation

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { truncate } = require('lodash');
+// const { truncate } = require('lodash');
+const generatePage = require('./src/template-page');
 
-// const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 
   const promptUser = () => {
@@ -139,6 +132,10 @@ const { truncate } = require('lodash');
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw err;
+    });
   });
 
